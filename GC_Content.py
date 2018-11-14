3c4bb939afeb1ac1b41dfaddd68eded98c5b6224
Ashipiling <Ashipiling@macdeiMac.local>
2018-11-14 15:57:20 +0800

    solution

--- GC_Content.py
+++ GC_Content.py
@@ -9,16 +9,17 @@ dict = defaultdict(float)
 ID = ''
 list = [0,0,0,0]
 for line in fasta.readlines():
-    if line.startswith('>'):
-        if not ID == '':
-            dict[ID] = float((list[1]+list[2])/(list[0]+list[1]+list[2]+list[3]))
-        ID = line.strip('>').split()[0]
-        list = [0,0,0,0]
     if not line.startswith('>'):
         list[0] += line.count('A')
         list[1] += line.count('C')
         list[2] += line.count('T')
         list[3] += line.count('G')
+    if line.startswith('>'):
+        if not ID == '':
+            dict[ID] = float((list[1]+list[2])/(list[0]+list[1]+list[2]+list[3]))
+        ID = line.strip('>').split()[0]
+        list = [0,0,0,0]
 dict[ID] = float((list[1]+list[2])/(list[0]+list[1]+list[2]+list[3]))
+
 dict1 = sorted(dict)
 print(dict1[0]+'\n'+str(dict[dict1[0]]))
