3cb6b279232e063b06010091bde44c5c9b8b84ef
Hao Luo <44961008+ashipiling@users.noreply.github.com>
2018-11-26 11:05:17 +0800

    Delete LEXF_EnumeratingLexicographically.py

--- LEXF_EnumeratingLexicographically.py
+++
@@ -1,7 +0,0 @@
-from itertools import product
-
-string = input().replace(' ', '').strip()
-n = int(input())
-
-for i in product(string, repeat=n):
-    print(str(i)[1:-1].replace("'","").replace(', ',''))
