0141a8309d59e0850690d7f1320e06f01f815549
LuoHao <3130100174@zju.edu.cn>
2018-12-31 13:57:18 +0800

    change algorithm

--- LCSQ_FindingaSharedSplicedMotif.py
+++ LCSQ_FindingaSharedSplicedMotif.py
@@ -1,37 +1,51 @@
 from CONS_ConsensusProfile import fasta_to_list_without_name
 import sys
 
-def sort_DNA(dnalist):
-    sort_list = [0 for i in range(len(DNAlist[1]))]
-    dict = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
-    for i in range(len(DNAlist[0])):
-        for j in range(len(DNAlist[1])):
+def sort_DNA(dnalist,dict):
+    sort_list = [0 for i in range(len(dnalist[1]))]
+
+    for i in range(len(dnalist[0])):
+        for j in range(len(dnalist[1])):
             n = 0
-            if DNAlist[0][i] == DNAlist[1][j]:
+            if dnalist[0][i] == dnalist[1][j]:
                 n += 1
-                if n > dict[DNAlist[0][i]]:
+                if n > dict[dnalist[0][i]]:
                     sort_list[j] += max(sort_list) + 1
-        dict[DNAlist[0][i]] += 1
+        dict[dnalist[0][i]] += 1
     return sort_list
 
-def longestIncreasingSubseq(list):
-    list1 = [[] for i in range(len(list))]
-    list2 = [[] for i in range(len(list))]
-    for i in range(len(list) - 1):
-        list1[i] = [list[i]]
-        list2[i].append(i)
-        for j in range(i, len(list)):
-            if int(list[j]) > int(max(list1[i], key=lambda x: int(x))):
-                list2[i].append(j)
-                list1[i].append(list[j])
-    return sorted(list2, key = lambda x:len(x),reverse= True)[0]
+def LIS_loc(list):
+    n = len(list)
+    end = 1
+    length = 1
+    p = [0 for i in range(n)]
+    l = [1 for j in range(n)]
+    for i in range(n):
+        for j in range(0, i):
+            if int(list[j]) < int(list[i]):
+                if l[i] < l[j] + 1:
+                    l[i] = l[j] + 1
+                    p[i] = j
+        if max(l) == l[i]:
+            length = l[i]
+            end = i
+    location = [0 for i in range(length)]
+    add = length - 1
+    location[add] = end
+    add = add-1
+    while add >= 0:
+        location[add] = p[end]
+        add = add - 1
+        end = p[end]
+    return location
 
 
 if __name__ == '__main__':
     #file = open('test.fasta')
+    dict = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
     file = sys.argv[1]
     DNAlist = fasta_to_list_without_name(file)
     final_seq = ''
-    for i in longestIncreasingSubseq(sort_DNA(DNAlist)):
+    for i in LIS_loc(sort_DNA(DNAlist,dict)):
         final_seq += DNAlist[0][i]
     print(final_seq)
